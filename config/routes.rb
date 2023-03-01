390ae573349a894cb1864f9fa7ab39c8559fa88c
Viktor Ruskai <viktor@ruskai.com>
2023-03-01 23:36:02 +0100

    feat(App): add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+  devise_for :users
+
+  root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
